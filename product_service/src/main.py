c9b6b0cc650bc5b99e9a3834f1500350744bb5d2
Praveen Kumar <Preveenkumar@fabc.global>
2025-12-07 21:49:41 +0530

    path changed for common_auth

--- product_service/src/main.py
+++ product_service/src/main.py
@@ -1,8 +1,7 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent))
-
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
