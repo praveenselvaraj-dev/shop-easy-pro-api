c1bd882ae83567c8a70de25f8db3eeae22838a67
Praveen Kumar <Preveenkumar@fabc.global>
2025-12-07 19:04:36 +0530

    DB Model Import Changed

--- user_service/src/infrastructure/database/models.py
+++ user_service/src/infrastructure/database/models.py
@@ -12,7 +12,7 @@ class UserModel(Base):
     """SQLAlchemy User Model"""
     __tablename__ = "users"
     __table_args__ = {"extend_existing": True}  # ✅ add this
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     username = Column(String(100), unique=True, index=True, nullable=False)  
     email = Column(String(255), unique=True, index=True, nullable=False)     
     password_hash = Column(String(255), nullable=False)                      
