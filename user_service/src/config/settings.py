af4bb2e02bc3fff4267619012f8e10c5432a277e
Praveen Kumar <Preveenkumar@fabc.global>
2025-12-07 16:32:22 +0530

    Change package name

--- user_service/src/config/settings.py
+++ user_service/src/config/settings.py
@@ -10,9 +10,9 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     
-    class Config:
-        env_file = ".env"
-        case_sensitive = True
+    # class Config:
+    #     env_file = ".env"
+    #     case_sensitive = True
 
 @lru_cache()
 def get_settings():
