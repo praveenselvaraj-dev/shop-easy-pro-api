d1c52769d87815b85e294a171f2bd875eb9c3d02
Praveen Kumar <Preveenkumar@fabc.global>
2025-12-07 19:20:02 +0530

    Removed creating tables in main.py

--- user_service/src/infrastructure/database/connection.py
+++ user_service/src/infrastructure/database/connection.py
@@ -1,14 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from src.config.settings import get_settings
+from src.utils.logger import setup_logger
+logger = setup_logger(__name__)
 
 settings = get_settings()
-
 engine = create_engine(
     settings.DATABASE_URL,
     pool_pre_ping=True, 
     echo=False
 )
+logger.info(f"Connected DB URL: {settings.DATABASE_URL}")
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
